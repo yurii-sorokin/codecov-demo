86718dd259bb39bbd259285e2acf26b89ac037d0
Yurii Sorokin <yurii.sorokin@q4inc.com>
2021-06-08 19:48:59 +0300

    Add more tests

--- src/parity.ts
+++ src/parity.ts
@@ -2,6 +2,10 @@ export function isEven(value: number) {
   return value % 2 === 0;
 }
 
+export function isOdd(value: number) {
+  return value % 2 !== 0;
+}
+
 export function getParity(value: number) {
-  return isEven(value) ? 'even' : 'odd'
+  return isEven(value) ? 'even' : 'odd';
 }
